2d2c5eb8313223b0628133138bdf11a4ba760283
Felipe Gentil <cdigentil@gmail.com>
2015-05-26 10:56:38 -0300

    Align assigments in parser

--- lib/uniara_virtual_parser/services/login.rb
+++ lib/uniara_virtual_parser/services/login.rb
@@ -3,7 +3,8 @@ module UniaraVirtualParser
     module Login
       def login(ra, password)
         response = Client.post('/login', username: ra, senha: password)
-        cookies = response.header['Set-Cookie']
+        cookies  = response.header['Set-Cookie']
+
         fail InvalidLogin unless /UVXS233E3=S/.match cookies
         cookies.match(/PHPSESSID=([^;]*);/)[1]
       end
