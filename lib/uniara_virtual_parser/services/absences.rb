3af864143fa188189183fed4f5bb3ef9b30747a4
Ezequiel Messore <ezequiel.messore@hotmail.com>
2016-01-13 21:19:11 -0200

    Update service/absence.rb

--- lib/uniara_virtual_parser/services/absences.rb
+++ lib/uniara_virtual_parser/services/absences.rb
@@ -9,21 +9,21 @@ module UniaraVirtualParser
 
 		private
 
-		def parse_absences(html)
-			doc = Nokogiri::HTML(html)
-			absences = []
-			subjects = []
-
-			doc.css('div#conteudo ~table tr').each_with_index do |absence,index|
-				next if index.zero?
-				absences << Models::Absence.new(
-					frequency: absence.css('td:nth-child(2)').text,
-					total:     absence.css('td:nth-child(3)').text
-				)
+			def parse_absences(html)
+				doc = Nokogiri::HTML(html)
+				absences = []
+				subjects = []
+	
+				doc.css('div#conteudo ~table tr').each_with_index do |absence,index|
+					next if index.zero?
+					absences << Models::Absence.new(
+						frequency: absence.css('td:nth-child(2)').text,
+						total:     absence.css('td:nth-child(3)').text
+					)
+				end
+				subjects << Models::Subject.new(absences:absences)
+				subjects
 			end
-			subjects << Models::Subject.new(absences:absences)
-			subjects
-		end
 		end
 	end
 end
